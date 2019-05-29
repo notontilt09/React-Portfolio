f2980059c5f39b0eea4d7774deb86c2547406a2d
Daniel Weinman <namniewd@gmail.com>
2019-05-29 13:33:33 -0400

    added app images instead of icons, some css adjusting

--- portfolio/src/components/Bio.js
+++ portfolio/src/components/Bio.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 import './Bio.css';
 
+import noise_controller from '../assets/noise-controller.png';
+import manaje from '../assets/manaje.png';
+import guidr from '../assets/guidr.png';
+import insta from '../assets/insta.png';
+
 class Bio extends React.Component {
     scrollToStuff = () => {
         const stuff = document.querySelector('.stuff-i-do');
@@ -62,31 +67,39 @@ class Bio extends React.Component {
                         <p>Past projects built using a combination of HTML/CSS for basic design, React/Redux to create interactive UI's, and Node.js/Express for the backend.</p>
                         <ul className="icons-grid">
                             <li>
-                                <a href="https://dan-calculator.netlify.com/" target='_blank' rel="noopener noreferrer">
-                                    <span className="icon fas fa-calculator"></span>
-                                    <h3 className="proj-desc">Calculator</h3>
-                                    <p>A fully functioning basic calculator built with React.js</p>
+                                <a href="https://manaje-refactor.netlify.com" className="project" target="_blank" rel="noopener noreferrer">
+                                    <img src={manaje} alt="manaje" className="screenshot screenshot-left"></img>
+                                    <div className="description description-right">
+                                        <h3 className="proj-desc">Manaje</h3>
+                                        <p>Refactored an existing codebase of a team management application using GraphQL to build the API, Prisma to interact with a postgres DB, and Apollo Client on the front end to interact with the GraphQL layer.  Added additional functionality to the app in the form of to do lists.</p>
+                                    </div>
                                 </a>
                             </li>
                             <li>
-                                <a href="https://dan-instagram.netlify.com/" target="_blank" rel="noopener noreferrer">
-                                    <span className="icon fab fa-instagram"></span>
-                                    <h3 className="proj-desc">Insta</h3>
-                                    <p>A project built to mimic an Instragram feed, complete with login, comments, and like/unlike functionality built with React.js</p>
+                                <a href="https://thenoisecontroller.netlify.com/" className="project flip" target='_blank' rel="noopener noreferrer">
+                                    <div className="description description-left">
+                                        <h3 className="proj-desc">Noise Controller</h3>
+                                        <p>An application for elementary school teachers to keep the noise level down in the classroom.  Led a team of developers using a React/Node/Express stack, and implemented extra game functionality using the native Javascript WebAudio API.</p>
+                                    </div>
+                                    <img src={noise_controller} alt="noise-controller" className="screenshot screenshot-right"></img>
                                 </a>
                             </li>
                             <li>
-                                <a href="https://guidr-react-dan.netlify.com/login" target="_blank" rel="noopener noreferrer">
-                                    <span className="icon fas fa-plane"></span>
-                                    <h3 className="proj-desc">Guidr</h3>
-                                    <p>An application for outdoor guides to arrange meetups with adventure seekers.  Front-end built with React.js and Redux and backend RESTful API built with Node.js and Express</p>
+                                <a href="https://guidr-react-dan.netlify.com/login" className="project" target="_blank" rel="noopener noreferrer">
+                                    <img src={guidr} alt="guidr" className="screenshot screenshot-left"></img>
+                                    <div className="description description-right">
+                                        <h3 className="proj-desc">Guidr</h3>
+                                        <p>An application for outdoor guides to arrange meetups with adventure seekers.  Front-end built with React.js and Redux and backend RESTful API built with Node.js and Express</p>
+                                    </div>
                                 </a>
                             </li>
                             <li>
-                                <a href="https://manaje-refactor.netlify.com" target="_blank" rel="noopener noreferrer">
-                                    <i className="icon fas fa-tasks"></i>
-                                    <h3 className="proj-desc">Manaje</h3>
-                                    <p>Refactored an existing codebase of a team management application using GraphQL to build the API, Prisma to interact with a postgres DB, and Apollo Client on the front end to interact with the GraphQL layer.  Added additional functionality to the app in the form of to do lists.</p>
+                                <a href="https://dan-instagram.netlify.com/" className="project flip" target="_blank" rel="noopener noreferrer">
+                                    <div className="description description-left">
+                                        <h3 className="proj-desc">Insta</h3>
+                                        <p>A project built to mimic an Instragram feed, complete with login, comments, and like/unlike functionality built with React.js</p>
+                                    </div>
+                                    <img src={insta} alt="insta" className="screenshot screenshot-right"></img>
                                 </a>
                             </li>
                         </ul>
